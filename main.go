6321cf5d322bc6a08d207844b58ecfed21f62d13
Hugo Haaxman <63868236+hhhug000@users.noreply.github.com>
2026-08-17 22:09:07 +0100

    Get domains to scrape from user input

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hhhug000/harvester/crawler"
 )
 
 func main() {
+	var domain string
+	fmt.Println("Choose a domain to scrape")
+	fmt.Scan(&domain)
 	crawlerEngine := crawler.NewEngine("./output")
-	crawlerEngine.Crawl("go.dev")
+	crawlerEngine.Crawl(domain)
 }
